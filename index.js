58f0cc632ba1f1840afb20c2928d57ae3ba91a29
Adam Duff <j.adam.duff@gmail.com>
2018-10-22 22:49:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function scuberGreetingForFeet(distance) {
 function ternaryCheckCity(city) {
   return city === "NYC" ? "Ok, sounds good." :  "No go."
 }
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+
+    case 'not as generous':
+      return "Thank you.";
+      break;
+
+    default:
+      return "Bye.";
+      break;
+  }
+}
